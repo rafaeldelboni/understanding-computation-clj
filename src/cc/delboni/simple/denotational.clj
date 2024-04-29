b1d6fbdc2d53319d5c2fa69f3e2e90ae0e5914bf
Rafael Delboni <rafadelboni@gmail.com>
2024-04-29 17:48:13 -0300

    feat: adds denotational expressions

--- src/cc/delboni/simple/denotational.clj
+++ src/cc/delboni/simple/denotational.clj
@@ -1,5 +1,9 @@
 (ns cc.delboni.simple.denotational)
 
+(defn invoke
+  ([f] (invoke f {}))
+  ([f e] (f e)))
+
 (defprotocol Expression
   (->clj [self]
     "Denotational semantics for Simple by translating it into Clojure."))
@@ -20,3 +24,56 @@
   (->clj [_]
     `(fn [environment#]
        (get environment# ~var-name))))
+
+; Expressions
+(defrecord Add [left right]
+  Expression
+  (->clj [_]
+    `(fn [environment#]
+       (+ (-> ~left ->clj eval (invoke environment#))
+          (-> ~right ->clj eval (invoke environment#))))))
+
+(defrecord Multiply [left right]
+  Expression
+  (->clj [_]
+    `(fn [environment#]
+       (* (-> ~left ->clj eval (invoke environment#))
+          (-> ~right ->clj eval (invoke environment#))))))
+
+(defrecord LessThan [left right]
+  Expression
+  (->clj [_]
+    `(fn [environment#]
+       (< (-> ~left ->clj eval (invoke environment#))
+          (-> ~right ->clj eval (invoke environment#))))))
+
+; Statements
+; (defrecord DoNothing []
+;   Expression
+;   (evaluate [_ environment]
+;     environment))
+;
+; (defrecord Assign [var-name expression]
+;   Expression
+;   (evaluate [_ environment]
+;     (assoc environment var-name (evaluate expression environment))))
+;
+; (defrecord If [condition consequence alternative]
+;   Expression
+;   (evaluate [_ environment]
+;     (if (:value (evaluate condition environment))
+;       (evaluate consequence environment)
+;       (evaluate alternative environment))))
+;
+; (defrecord Sequence [one two]
+;   Expression
+;   (evaluate [_ environment]
+;     (evaluate two (evaluate one environment))))
+;
+; (defrecord While [condition body]
+;   Expression
+;   (evaluate [_ environment]
+;     (loop [env environment]
+;       (if-not (:value (evaluate condition env))
+;         env
+;         (recur (evaluate body env))))))
--- test/cc/delboni/simple/denotational_test.clj
+++ test/cc/delboni/simple/denotational_test.clj
@@ -1,12 +1,9 @@
 (ns cc.delboni.simple.denotational-test
-  (:require [cc.delboni.simple.denotational :refer [->Bool ->Numeric ->Variable
-                                                    ->clj]]
+  (:require [cc.delboni.simple.denotational :refer [->Add ->Bool ->Bool ->clj
+                                                    ->LessThan ->Numeric ->Multiply
+                                                    ->Variable invoke]]
             [clojure.test :refer [deftest is testing]]
             [matcher-combinators.test :refer [match?]]))
-
-(defn ^:private invoke
-  ([f] (invoke f {}))
-  ([f e] (f e)))
 
 (deftest values-test
   (testing "should evaluate values"
@@ -27,3 +24,49 @@
                     ->clj
                     eval
                     (invoke {:x 23}))))))
+
+(deftest expressions-test
+  (testing "should evaluate expressions"
+    (is (match? 6
+                (-> (->Add (->Numeric 3) (->Numeric 3))
+                    ->clj
+                    eval
+                    invoke)))
+
+    (is (match? 9
+                (-> (->Multiply (->Numeric 3) (->Numeric 3))
+                    ->clj
+                    eval
+                    invoke)))
+
+    (is (match? true
+                (-> (->LessThan (->Numeric 1) (->Numeric 3))
+                    ->clj
+                    eval
+                    invoke)))
+
+    (is (match? false
+                (-> (->LessThan (->Numeric 4) (->Numeric 3))
+                    ->clj
+                    eval
+                    invoke)))
+
+    (is (match? true
+                (-> (->LessThan (->Add (->Variable :x) (->Numeric 2))
+                                (->Variable :y))
+                    ->clj
+                    eval
+                    (invoke {:x 2 :y 5}))))
+
+    (is (match? 4
+                (-> (->Add (->Variable :x) (->Numeric 1))
+                    ->clj
+                    eval
+                    (invoke {:x 3}))))
+
+    (is (match? false
+                (-> (->LessThan (->Add (->Variable :x) (->Numeric 1))
+                                (->Numeric 3))
+                    ->clj
+                    eval
+                    (invoke {:x 3}))))))
