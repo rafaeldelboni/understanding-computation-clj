3e65eba37cad744c299f3e494600ed345f8a6c37
Rafael Delboni <rafadelboni@gmail.com>
2024-05-24 13:19:23 -0300

    feat(automata): adds basic dfa

--- src/cc/delboni/simple_computers/dfa.clj
+++ src/cc/delboni/simple_computers/dfa.clj
@@ -8,6 +8,11 @@
   (rule-for [self state character])
   (next-state [self state character]))
 
+(defprotocol DFAProtocol
+  (accepting? [self])
+  (read-char [self character])
+  (read-str [self string]))
+
 (defrecord FARule [state character next-state]
   Object
   (toString [_]
@@ -23,8 +28,22 @@
 (defrecord DFARulebook [rules]
   Rulebook
   (rule-for [_ state character]
-    (some (fn [rule] (if (applies-to? rule state character) rule nil)) rules))
+    (some (fn [rule] (when (applies-to? rule state character) rule)) rules))
 
   (next-state [this state character]
     (-> (rule-for this state character)
         follow)))
+
+(defrecord DFA [current-state accept-states rulebook]
+  DFAProtocol
+  (accepting? [_]
+    (boolean (some #(= % current-state) accept-states)))
+
+  (read-char [self character]
+    (->DFA (next-state (:rulebook self) current-state character)
+           (:accept-states self)
+           (:rulebook self)))
+
+  (read-str [self string]
+    (reduce
+     (fn [acc cur] (read-char acc cur)) self (vec string))))
--- test/cc/delboni/simple_computers/dfa_test.clj
+++ test/cc/delboni/simple_computers/dfa_test.clj
@@ -1,20 +1,60 @@
 (ns cc.delboni.simple-computers.dfa-test
-  (:require [cc.delboni.simple-computers.dfa :refer [->DFARulebook ->FARule
-                                                     next-state]]
+  (:require [cc.delboni.simple-computers.dfa :refer [->DFA ->DFARulebook
+                                                     ->FARule accepting?
+                                                     next-state read-char
+                                                     read-str]]
             [clojure.test :refer [deftest is testing]]
             [matcher-combinators.test :refer [match?]]))
 
-(deftest values-test
-  (testing "should evaluate values"
-    (let [rulebook (->DFARulebook
-                    [(->FARule 1 'a' 2) (->FARule 1 'b' 1)
-                     (->FARule 2 'a' 2) (->FARule 2 'b' 3)
-                     (->FARule 3 'a' 3) (->FARule 3 'b' 3)])]
-      (is (match? 2
-                  (next-state rulebook 1 'a')))
+(def rulebook
+  (->DFARulebook
+   [(->FARule 1 \a 2) (->FARule 1 \b 1)
+    (->FARule 2 \a 2) (->FARule 2 \b 3)
+    (->FARule 3 \a 3) (->FARule 3 \b 3)]))
 
-      (is (match? 1
-                  (next-state rulebook 1 'b')))
+(deftest next-state-test
+  (testing "should evaluate next-state"
+    (is (match? 2
+                (next-state rulebook 1 \a)))
 
-      (is (match? 3
-                  (next-state rulebook 2 'b'))))))
+    (is (match? 1
+                (next-state rulebook 1 \b)))
+
+    (is (match? 3
+                (next-state rulebook 2 \b)))))
+
+(deftest accepting-test
+  (testing "should accept or not an state"
+    (is (match? true
+                (-> (->DFA 1 [1 3] rulebook)
+                    accepting?)))
+    (is (match? false
+                (-> (->DFA 1 [3] rulebook)
+                    accepting?)))
+
+    (is (match? false
+                (-> (->DFA 1 [3] rulebook)
+                    (read-char \b)
+                    accepting?)))
+
+    (is (match? false
+                (-> (->DFA 1 [3] rulebook)
+                    (read-char \b)
+                    (read-char \a)
+                    (read-char \a)
+                    (read-char \a)
+                    accepting?)))
+
+    (is (match? true
+                (-> (->DFA 1 [3] rulebook)
+                    (read-char \b)
+                    (read-char \a)
+                    (read-char \a)
+                    (read-char \a)
+                    (read-char \b)
+                    accepting?)))
+
+    (is (match? true
+                (-> (->DFA 1 [3] rulebook)
+                    (read-str "baaab")
+                    accepting?)))))
