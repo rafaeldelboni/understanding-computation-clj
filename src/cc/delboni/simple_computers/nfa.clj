39d5fa9d29d4b2da42963ee1627668030c7482c0
Rafael Delboni <rafadelboni@gmail.com>
2024-06-25 17:32:26 -0300

    feat(nfa): adds implementation of Nondeterminism

--- src/cc/delboni/simple_computers/nfa.clj
+++ src/cc/delboni/simple_computers/nfa.clj
@@ -1,10 +1,20 @@
 (ns cc.delboni.simple-computers.nfa
-  (:require [cc.delboni.simple-computers.fa :as fa]))
+  (:require [cc.delboni.simple-computers.fa :as fa]
+            [clojure.set :as set]))
 
 (defprotocol NFARulebookProtocol
   (rules-for [self state character])
   (follow-rules-for [self state character])
-  (next-state [self states character]))
+  (next-states [self states character]))
+
+(defprotocol NFAProtocol
+  (accepting? [self])
+  (read-char [self character])
+  (read-str [self string]))
+
+(defprotocol NFADesignProtocol
+  (to-nfa [self])
+  (accepts? [self string]))
 
 (defrecord NFARulebook [rules]
   NFARulebookProtocol
@@ -16,8 +26,35 @@
          (rules-for this state)
          (map fa/follow)))
 
-  (next-state [this states character]
+  (next-states [this states character]
     (->> states
          (map (fn [state] (follow-rules-for this state character)))
          flatten
          set)))
+
+(defrecord NFA [current-states accept-states rulebook]
+  NFAProtocol
+  (accepting? [_]
+    (-> (set/intersection (set current-states) (set accept-states))
+        seq
+        boolean))
+
+  (read-char [self character]
+    (->NFA (next-states (:rulebook self) (set current-states) character)
+           (:accept-states self)
+           (:rulebook self)))
+
+  (read-str [self string]
+    (reduce
+     (fn [acc cur] (read-char acc cur)) self (vec string))))
+
+(defrecord NFADesign [start-state accept-states rulebook]
+  NFADesignProtocol
+  (to-nfa [self]
+    (->NFA (hash-set (:start-state self)) (:accept-states self) (:rulebook self)))
+
+  (accepts? [self string]
+    (-> (to-nfa self)
+        (read-str string)
+        accepting?)))
+
--- test/cc/delboni/simple_computers/nfa_test.clj
+++ test/cc/delboni/simple_computers/nfa_test.clj
@@ -1,7 +1,10 @@
 (ns cc.delboni.simple-computers.nfa-test
-  (:require [cc.delboni.simple-computers.nfa :refer [->NFARulebook
-                                                     next-state]]
-            [cc.delboni.simple-computers.fa :refer [->FARule]]
+  (:require [cc.delboni.simple-computers.fa :refer [->FARule]]
+            [cc.delboni.simple-computers.nfa :refer [->NFA ->NFARulebook
+                                                     ->NFADesign
+                                                     accepting? accepts?
+                                                     next-states read-char
+                                                     read-str]]
             [clojure.test :refer [deftest is testing]]
             [matcher-combinators.test :refer [match?]]))
 
@@ -14,10 +17,55 @@
 (deftest next-state-test
   (testing "should evaluate next-state"
     (is (match? #{1 2}
-                (next-state rulebook #{1} \b)))
+                (next-states rulebook #{1} \b)))
 
     (is (match? #{1 3}
-                (next-state rulebook #{1 2} \a)))
+                (next-states rulebook #{1 2} \a)))
 
     (is (match? #{1 2 4}
-                (next-state rulebook #{1 3} \b)))))
+                (next-states rulebook #{1 3} \b)))))
+
+(deftest accepting-test
+  (testing "should accept or not an list of possible states vs possible acceptances"
+    (is (match? false
+                (-> (->NFA #{1} #{4} rulebook)
+                    accepting?)))
+
+    (is (match? true
+                (-> (->NFA #{1 2 4} #{4} rulebook)
+                    accepting?)))
+
+    (is (match? false
+                (-> (->NFA #{1} #{4} rulebook)
+                    (read-char \b)
+                    accepting?)))
+
+    (is (match? false
+                (-> (->NFA #{1} #{4} rulebook)
+                    (read-char \b)
+                    (read-char \a)
+                    accepting?)))
+
+    (is (match? true
+                (-> (->NFA #{1} #{4} rulebook)
+                    (read-char \b)
+                    (read-char \a)
+                    (read-char \b)
+                    accepting?)))
+
+    (is (match? true
+                (-> (->NFA #{1} #{3} rulebook)
+                    (read-str "bbbbb")
+                    accepting?)))))
+
+(deftest nfa-design-test
+  (testing "should create new nfa from start and read-str to put into a new state"
+    (let [nfa-design (->NFADesign 1 #{4} rulebook)]
+      (is (match? true
+                  (accepts? nfa-design "bab")))
+
+      (is (match? true
+                  (accepts? nfa-design "bbbbb")))
+
+      (is (match? false
+                  (accepts? nfa-design "bbabb"))))))
